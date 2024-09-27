1fe26d91ef11eed5e5d2d1b299f5427799537a9f
Hawraa Hijazi <hawraahijazi1996@gmail.com>
2024-09-27 06:09:46 +0000

    add event listener to the document

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+// Wait for the DOM to finish loading before answering
+// Get the button elements and add event listeners to them
+ 
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    buttons.addEventListener("click", function() {
+})
+
+})
+
 function checkAnswer () {
 
 }
