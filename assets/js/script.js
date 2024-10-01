5f6a7b0164b059731d15d788ef78c02cf1c74202
Hawraa Hijazi <hawraahijazi1996@gmail.com>
2024-10-01 11:37:37 +0000

    add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,32 @@
 // Wait for the DOM to finish loading before answering
 // Get the button elements and add event listeners to them
  
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
-    button.addEventListener("click", function() {
-})
+document.addEventListener("DOMContentLoaded", function(){
 
 })
 
 function checkAnswer () {
 
+ 
 }
 
 function clickSubmit () {
 
 }
-
+/** 
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore () {
-
+    let oldScore = parseInt(document.getElementbyId("correct").innerText);
+    document.getElementById("correct").innerText = ++oldScore;
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer () {
+    let oldScore = parseInt(document.getElementbyId("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
