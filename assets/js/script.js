da3060bc178a933d9bbea355319b9414c44cba8d
Hawraa Hijazi <hawraahijazi1996@gmail.com>
2024-10-07 05:49:16 +0000

    Configuration of the background of correct and wrong answers in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
-// Wait for the DOM to finish loading before answering
-// Get the button elements and add event listeners to them
- 
+/**
+ *  Wait for the DOM to finish loading before answering 
+ * Get the button elements and add event listeners to them
+ */
+
 document.addEventListener("DOMContentLoaded", function(){
     let buttons = document.getElementsByTagName("button"); 
     for (let button of buttons) {
         if (button.innerText === "SUBMIT ANSWER"){
-            button.addEventListener("click", function() {
+            button.addEventListener("click", function(event) {
                 event.preventDefault();
                 checkAnswer(button);
-             })
+             });
          }
     }
-})
+});
 /**
  * Control to check the capital of multiples countries
  */
@@ -23,11 +25,11 @@ function checkAnswer (button) {
         let correctAnswer = button.value;
         let evaluation = userAnswer === correctAnswer;
         if (evaluation) {
-            selectedbutton.parentElement.style.color = "green";
+            selectedbutton.parentElement.parentElement.style.color = "green";
             alert("Hey! Your answer was correct");
             incrementScore();
         } else {
-            selectedbutton.parentElement.style.color = "red";
+            selectedbutton.parentElement.parentElement.style.color = "red";
             alert(`Ohhhhh.... you answered ${userAnswer} . The correct answer was ${correctAnswer} .`);
             incrementWrongAnswer();
         }
@@ -41,21 +43,6 @@ function checkAnswer (button) {
         alert('Select an option!');
     } 
 }
-/**
- * Show image when the user answered correctly
- */
-/** 
-function showImage (selectedbutton,submitbutton,image) {
-     let correctAnswer = submitbutton.value;
-     let userAnswer = selectedbutton.value;
-     let evaluation = correctAnswer === userAnswer;
-     if (evaluation) {
-        image.id.style.display= 'inline-block';
-     } else {
-        image.id.style.display= 'none';
-     }
-}
-*/
 /** 
  * Gets the current score from the DOM and increments it by 1
  */
@@ -71,29 +58,29 @@ function incrementWrongAnswer () {
     let oldScore = parseInt(document.getElementById("incorrect").innerText);
     document.getElementById("incorrect").innerText = ++oldScore;
 }
-document.getElementById("resetbutton").addEventListener("click", function() {
-    /** 
-     * Clear all selected radio buttons by resetting the forms
-     */
+/** 
+ * Clear all selected radio buttons by resetting the forms
+ */
+document.getElementById("resetbutton").addEventListener("click", function(event) {
     event.preventDefault();
     let forms = document.getElementsByTagName("form");
-    for (let form of forms){
-      form.reset();
-      let division = document.getElementsByClassName('answergroup');
-      division.style.color = "white";
-    };
+    for (let form of forms) {
+      form.reset(); 
+      form.style.color = "white";
+    }
+    
     /** 
      * Reset all buttons
      */
     let buttons = document.getElementsByTagName("button"); 
     for (let button of buttons) {
       if (button.innerText != "RESET"){
-      button.disabled = false;
-      button.innerText = "SUBMIT ANSWER";
-      button.style.backgroundColor = "#445361";
-      button.style.color = "white"; 
-      button.style.cursor = "pointer"; 
-      }
+            button.disabled = false;
+            button.innerText = "SUBMIT ANSWER";
+            button.style.backgroundColor = "#445361";
+            button.style.color = "white"; 
+            button.style.cursor = "pointer"; 
+        }
     }
     /**
      * Reset the scores
@@ -102,4 +89,3 @@ document.getElementById("resetbutton").addEventListener("click", function() {
     document.getElementById("correct").innerText = 0;
  
 });
-
